bdc02aa4acbf216b86a3a200cfbc062f047ecbb1
Kort <r.benard87@gmail.com>
2018-09-30 22:41:00 +0200

    Fin pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+
+    const date = new Date();
+
+    setTimeout(
+
+      () => {
+
+        resolve(date);
+
+      }, 2000
+
+    );
+
+  });
 
   appareils = [
     {
