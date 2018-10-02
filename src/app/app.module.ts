a9c83d0052621b0e3d2fba8fe65594419a4c4c7f
Kort <r.benard87@gmail.com>
2018-10-02 19:38:36 +0200

    Fin chapitre forms méthode template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { AuthService } from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuardService } from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 const appRoutes: Route[] = [
   { path: 'appareils', canActivate: [AuthGuardService], component: AppareilViewComponent },
   { path: 'appareils/:id', canActivate: [AuthGuardService], component: SingleAppareilComponent },
+  { path: 'edit', canActivate: [AuthGuardService], component: EditAppareilComponent },
   { path: 'auth', component: AuthComponent },
   { path: '', component: AuthComponent },
   { path: 'not-found', component: FourOhFourComponent },
@@ -32,7 +34,8 @@ const appRoutes: Route[] = [
     AuthComponent,
     AppareilViewComponent,
     SingleAppareilComponent,
-    FourOhFourComponent
+    FourOhFourComponent,
+    EditAppareilComponent
   ],
   imports: [
     BrowserModule,
